6a5b0c9ae25178a6802b90c8f05bfe174a13bcc2
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-09-28 15:31:26 +0545

    Revision

---
+++ codes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int a=17, b=4;
+    printf("sum =%d\n", a+b);
+    printf("difference= %d\n",a-b);
+    printf("product=%d\n", a*b);
+    printf("quotient=%d\n",a/b);
+    printf("remainder=%d\n",a%b);
+    
+}
