26997b3c2e13f7bf8f466b2943babc80e0516bef
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-09-30 09:23:26 +0545

    calculating sum of digits

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+    int num,rem,sum=0;
+    printf("enter number for num:\n");
+    scanf("%d",&num);
+    while (num>0)
+    {
+        rem= num%10;
+        sum= sum+rem;
+        num= num/10;
+
+    }
+    printf("sum of the digits= %d\n",sum);
+
+}
