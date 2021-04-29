d587f75a84f12891b86a3fa95890122db4e7602c
sheelaadhikari <adhikarisheela1000@gmail.com>
2021-04-29 07:44:30 +0545

    union

--- POINTERS/new.c
+++ POINTERS/new.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
-void main(){
-    printf("just test");
-}
+void accept(int x);
+    void main(){
+        int a;
+        printf("enter the number: ");
+        scanf("%d", &a);
+        accept(a);
+
+    }
+    void accept(int x){
+        if(x>9&&x<100)
+        printf("accepted");
+        else
+        printf("error");
+    }
+
