0e06cc60c8f0d182964e33ccc2d7ea6b40690839
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-05-19 18:59:12 +0545

    Add files via upload

---
+++ swap.php
@@ -0,0 +1,11 @@
+<?php
+$a = 45;
+$b = 12;
+echo "\nBefore swapping is:\n ";
+echo "Number a=".$a." and b=".$b; 
+$c = $a;
+$a = $b;
+$b = $c;
+echo "After swapping is:\n";
+echo "Number a=".$a." and b=".$b."\n";
+?>
